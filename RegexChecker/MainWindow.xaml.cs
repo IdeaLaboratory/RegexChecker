8dbb69ebcb0cf742bd7f5d5d5401964d142ad606
AdiTCG <adithecomputerguy@gmail.com>
2016-04-26 21:34:42 +0530

    Regex checker is a very small but useful utility. It is facilitate developer to quickly check regular expression he going to use. I used to use this tool providing quick check of the regex pattern you want to use for validation.

---
+++ RegexChecker/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RegexChecker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            InpurStringTB.BorderBrush = Brushes.Red;
+        }
+
+        private void checkBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(Convert.ToString(RegexTB.Text)))
+            {
+                status.Text = "Enter Regex";
+                return;
+            }
+            if (String.IsNullOrEmpty(InpurStringTB.Text))
+            {
+                status.Text = "Input string";
+                return;
+            }
+            //.Replace('\\', '\')
+            if (IsRegexPatternValid(RegexTB.Text))
+            {
+                Regex regX = new Regex(RegexTB.Text);
+                bool IsValidString = regX.IsMatch(InpurStringTB.Text);
+                if (IsValidString)
+                {
+                    status.Text = "True";
+                    status.Background = Brushes.LightGreen;
+                }
+                else
+                {
+                    status.Text = "False";
+                    status.Background = Brushes.LightCyan;
+                }
+                //MessageBox.Show(IsValidString.ToString());
+            }
+            else
+            {
+                status.Text = "Invalid regex pattern";
+                status.Background = Brushes.LightGoldenrodYellow;
+            }
+
+        }
+
+        private bool IsRegexPatternValid(String pattern)
+        {
+            try
+            {
+                new Regex(pattern);
+                return true;
+            }
+            catch { }
+            return false;
+        }
+    }
+}
